44be6ca08793825d5627fe3c1d53bd917909d0f9
sonali-mahidhariya <144592495+sonali-mahidhariya@users.noreply.github.com>
2023-10-27 22:30:08 +0530

    Develop a PHP script to fill an array with a specific value using the array_fill() function.

---
+++ Task_3/script_46.php
@@ -0,0 +1,13 @@
+<?php
+echo "lab division of classB";
+echo "<br>";
+echo "b1 lab 1 t0 30 roll_no";
+echo "<br>";
+$division=array_fill(1,30,"b1");
+print_r($division);
+echo "<br>";
+echo "b2 lab 31 to 50 roll_no";
+echo "<br>";
+$division1=array_fill(31,20,"b2");
+print_r($division1);
+?>
