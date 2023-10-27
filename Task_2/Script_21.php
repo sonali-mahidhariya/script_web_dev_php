00d44f931a81dbb8725e257966eb808c89c5ef56
sonali-mahidhariya <144592495+sonali-mahidhariya@users.noreply.github.com>
2023-10-27 21:35:42 +0530

    Script to check if a given number is a palindrome or not and display the result.

---
+++ Task_2/Script_21.php
@@ -0,0 +1,21 @@
+<?php
+$num = 181;
+$p=$num;
+$revnum = 0;
+
+while ($num != 0)
+{
+	$revnum = $revnum * 10 + $num % 10;
+
+	$num = (int)($num / 10); 
+}
+if($revnum==$p)
+{
+	echo $p," is Palindrome number";
+}
+else
+{
+	echo $p," is not Palindrome number";
+}
+
+?>
