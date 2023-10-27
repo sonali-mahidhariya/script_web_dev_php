c4cccbe95dc4925beb55880dd09a87ff9aac89db
sonali-mahidhariya <144592495+sonali-mahidhariya@users.noreply.github.com>
2023-10-27 22:38:01 +0530

    Develop a PHP script that uses the array_key_first() and array_key_last() functions to retrieve the first and last keys of an array.

---
+++ Task_3/script_55.php
@@ -0,0 +1,11 @@
+<?php 
+echo "last key of araay";
+echo "<br>";
+$color= array("o"=>"orange","p"=>"pink","g"=>"green"); 
+print_r(array_key_last($color));
+echo "<br>";
+echo "first key of araay";
+echo "<br>";
+$color= array("o"=>"orange","p"=>"pink","g"=>"green"); 
+print_r(array_key_first($color));
+?>
